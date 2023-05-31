b13564a6b7f5dd4661f08867e59a92218e691f56
lutfi-pratama <pratamalutfi60@gmail.com>
2023-05-31 08:05:45 +0700

    add search input bar

--- home.js
+++ home.js
@@ -1,44 +1,71 @@
 const galleryMenu = document.querySelectorAll(".nav .nav-item a");
 
-function startingLoad() {
-  let activeStartMenu = document.querySelectorAll(".tab-content .active");
-  getAllImages(activeStartMenu[0].id, activeStartMenu[0]);
-}
-
-function getAllImages(id, element) {
+function getImages(dummyData) {
   let imgColTag = "";
-  for (let i = 1; i <= 4; i++) {
+  dummyData.forEach(data => {
     imgColTag = imgColTag.concat(
       " ",
       `
-        <div class="col-lg-3 img-container">
+        <div class="col-sm-6 col-md-4 col-lg-3 img-container mb-4">
           <img
             class="img-gallery shadow-lg rounded-4"
             id="img-robot"
-            src="https://robohash.org/${i}/.png"
-            alt=""
+            src=${data.img}
           />
-          <div class="img-title rounded-4">Robot ${i}</div>
+          <div class="img-title rounded-4">${data.title}</div>
         </div>
       `
     );
-  }
+  });
 
+  return imgColTag;
+}
+
+function getImagesByCategory(id, element) {
   switch (id) {
     case "content-robot":
+      let robotFiltered = dummy.filter(data => data.category === "robot");
+
       element.innerHTML = `    
         <div class="row text-center mt-4">
-          ${imgColTag}
+          ${getImages(robotFiltered)}
         </div>
       `;
       break;
     case "content-nature":
-      element.innerHTML = "ini nature";
+      let natureFiltered = dummy.filter(data => data.category === "nature");
+
+      element.innerHTML = `    
+        <div class="row text-center mt-4">
+          ${getImages(natureFiltered)}
+        </div>
+      `;
       break;
 
     default:
       break;
   }
+}
+
+function getImagesByName(name, element) {
+  let imagesNameFiltered = [];
+
+  dummy.forEach(data => {
+    if (data.title.includes(name)) {
+      imagesNameFiltered.push(data);
+    }
+  });
+
+  element.innerHTML = `    
+    <div class="row text-center mt-4">
+      ${getImages(imagesNameFiltered)}
+    </div>
+  `;
+}
+
+function startingLoad() {
+  let activeStartMenu = document.querySelectorAll(".tab-content .active");
+  getImagesByCategory(activeStartMenu[0].id, activeStartMenu[0]);
 }
 
 startingLoad();
@@ -48,7 +75,25 @@ galleryMenu.forEach(menu => {
     // galleryMenu.forEach(menu => {
     //   menu.classList.remove("active");
     // });
-
-    getAllImages(activeMenu[0].id, activeMenu[0]);
+    getImagesByCategory(activeMenu[0].id, activeMenu[0]);
   });
+});
+
+const inputSearch = document.querySelector("#input-search");
+const carousel = document.querySelector(".carousel");
+const mainSection = document.querySelector("#main-section");
+
+// get value from input search and send to function
+inputSearch.addEventListener("keyup", () => {
+  let value = inputSearch.value;
+
+  if (value !== "") {
+    carousel.classList.add("d-none");
+  } else {
+    carousel.classList.remove("d-none");
+  }
+
+  console.log(value);
+
+  // trigger refresh value
 });
